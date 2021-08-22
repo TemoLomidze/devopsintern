a54ca6222095d79b93575e8dd6aaeb89f84d14ac
TemoLomidze <temo@lomidze.ge>
2021-08-22 13:58:36 +0400

    task6

--- Task6/docker.sh
+++ Task6/docker.sh
@@ -3,8 +3,6 @@
 #update system
 sudo apt-get update -y
 
-sleep 5
-
 sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
@@ -12,7 +10,7 @@ sudo apt-get -y install \
     gnupg \
     lsb-release
 
-sleep5
+sleep 5
 
 #Add docker repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -24,19 +22,26 @@ echo \
 sleep 3
 
 #Update repos and install Docker
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+#Give Docker permissions to run without sudo
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo service docker restart
+
 
 sleep 3
 
 #install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sleep 1
+sleep 3
 sudo chmod +x /usr/local/bin/docker-compose
+
+sleep 3
 
 #Test install
 docker-compose --version
-
 
 
 
